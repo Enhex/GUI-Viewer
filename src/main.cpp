8aea7f198afe29d67abbd474c38227fd05c7fcc8
Enhex <enhex0@gmail.com>
2019-01-04 18:11:28 +0200

    WIP: file path fields

--- src/main.cpp
+++ src/main.cpp
@@ -41,67 +41,6 @@ int main()
 	app.load_layout("gui/layout.deco", app.root);
 
 	app.root.child_layout->perform();
-	
-
-	/*TODO
-	style file should be included from the layout file?
-	how the layout file is included?
-	have separate entry points for style and layout?
-	let the user specify the file paths?
-		- style/layout path is relative to the working directory path
-	*/
-	{
-		auto file = std::ifstream("style.deco", std::ios::binary);
-		std::string file_str{
-			std::istreambuf_iterator<char>(file),
-			std::istreambuf_iterator<char>() };
-
-		auto stream = deco::make_InputStream(file_str.cbegin());
-
-		while (stream.position != file_str.cend() && !stream.peek_list_end())
-		{
-			std::string name;
-			style::style_st properties;
-			deco::serialize(stream, deco::begin_list(name));
-			deco::serialize(stream, properties);
-			deco::serialize(stream, deco::end_list);
-
-			app.style_manager.styles.emplace(name, properties);
-		}
-	}
-
-
-	// test using layout file
-	// read from file
-	{
-		auto file = std::ifstream("layout.deco", std::ios::binary);
-		std::string file_str{
-			std::istreambuf_iterator<char>(file),
-			std::istreambuf_iterator<char>() };
-
-		auto entries = deco::parse(file_str.begin(), file_str.end());
-		
-		//TODO this functionality should be provided by the GUI library
-		for (auto& entry : entries)
-		{
-			using namespace deco;
-
-			auto const& name = entry.content;
-
-			if (name == "element") {
-				auto& child = emplace_back_derived<element>(app.root.children);
-				read(entry, child);
-			}
-			else if (name == "panel") {
-				auto& child = emplace_back_derived<panel>(app.root.children);
-				read(entry, child);
-			}
-			else if (name == "text") {
-				auto& child = emplace_back_derived<text>(app.root.children);
-				read(entry, child);
-			}
-		}
-	}
 
 
 	// Loop until the user closes the window
